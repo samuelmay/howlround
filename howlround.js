3c4ae0999cffa6480207b9252ab0c1efdda25ad2
Samuel May <smay@1-stop.biz>
2021-07-18 23:10:24 +1000

    Get convolution to work using basic Gaussian kernel

--- howlround.js
+++ howlround.js
@@ -2,16 +2,124 @@ var ScreenResolution = {
     width: 768,
     height: 576
 };
+var FilterGain = 1;
+var FilterKernel = [
+    1, 4, 7, 4, 1,
+    4, 16, 26, 16, 4,
+    7, 26, 41, 26, 7,
+    4, 16, 26, 16, 4,
+    1, 4, 7, 4, 1
+].map(function (x) { return FilterGain * (x / 271); });
+var KernelRadius = Math.floor(Math.sqrt(FilterKernel.length) / 2);
+function getPixelPointer(x, y, imageData) {
+    // this is the number of array cells per pixel. There are four:
+    // red, green, blue, alpha
+    var rowLength = imageData.width;
+    var pixelDataSize = 4;
+    return y * (rowLength * pixelDataSize) + (x * pixelDataSize);
+}
+function getPixel(x, y, canvas) {
+    var imageData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
+    var pointer = getPixelPointer(x, y, imageData);
+    var red = imageData.data[pointer];
+    var green = imageData.data[pointer + 1];
+    var blue = imageData.data[pointer + 2];
+    var alpha = imageData.data[pointer + 3];
+    return [red, green, blue, alpha];
+}
+function getBrightness(pointer, imageData) {
+    var alpha = imageData.data[pointer + 3];
+    return alpha;
+}
+function setBrightness(pointer, imageData, brightness) {
+    imageData.data[pointer + 3] = brightness;
+}
+// this has side effects on the imageData array.
+function addBrightness(pointer, imageData, brightness) {
+    var value = getBrightness(pointer, imageData);
+    value = value + brightness;
+    // clip signal output
+    if (value > 255) {
+        value = 255;
+    }
+    setBrightness(pointer, imageData, value);
+}
+function testFilter(xI, yI, kernel) {
+    var k = 0;
+    for (var y = (yI - KernelRadius); y <= (yI + KernelRadius); y++) {
+        for (var x = (xI - KernelRadius); x <= (xI + KernelRadius); x++) {
+            // only operate when the kernel is not overlapping the
+            // edge of the image
+            if (y >= 0 && x >= 0 && y < ScreenResolution.height && x < ScreenResolution.width) {
+                console.log('(' + x.toString() + ',' + y.toString() + ')' + ' at ' + k.toString());
+            }
+            k++;
+            if (k > kernel.length) {
+                console.log('you fucked up');
+                return;
+            }
+        }
+    }
+}
+// This has side effects on the output data array.
+// The 'I' in 'xI' is for impulse.
+function filterPixel(xI, yI, inputData, kernel, outputData) {
+    var pointer = getPixelPointer(xI, yI, inputData);
+    var inputVal = getBrightness(pointer, inputData);
+    var k = 0;
+    for (var y = (yI - KernelRadius); y <= (yI + KernelRadius); y++) {
+        for (var x = (xI - KernelRadius); x <= (xI + KernelRadius); x++) {
+            // only operate when the kernel is not overlapping the
+            // edge of the image
+            if (y >= 0 && x >= 0 && y < inputData.height && x < inputData.width) {
+                var outputVal = inputVal * kernel[k];
+                pointer = getPixelPointer(x, y, outputData);
+                addBrightness(pointer, outputData, outputVal);
+            }
+            k++;
+            if (k > kernel.length) {
+                console.log('you fucked up');
+                return;
+            }
+        }
+    }
+}
+function convolution(inputData, kernel, outputData) {
+    var x = 0;
+    var y = 0;
+    for (y = 0; y < inputData.height; y++) {
+        for (x = 0; x < inputData.width; x++) {
+            filterPixel(x, y, inputData, kernel, outputData);
+            //let pointer = getPixelPointer(x, y, inputData);
+            //let currentValue = getBrightness(pointer, inputData);
+            //setBrightness(pointer, outputData, currentValue);
+            //addBrightness(pointer, outputData, 10); 
+        }
+    }
+}
+function feedback() {
+    console.log('feeding back...');
+    var tv = document.getElementById("tvscreen");
+    var ctx = tv.getContext("2d");
+    var thisIteration = ctx.getImageData(0, 0, tv.width, tv.height);
+    var nextIteration = ctx.createImageData(tv.width, tv.height);
+    convolution(thisIteration, FilterKernel, nextIteration);
+    ctx.putImageData(nextIteration, 0, 0);
+}
 function setupScreen() {
+    // initial content
+    //
     var tv = document.getElementById("tvscreen");
     // Note that for the canvas element, width and height
     // are *different* to the css/style width and height.
     tv.width = ScreenResolution.width;
     tv.height = ScreenResolution.height;
     var ctx = tv.getContext("2d");
-    ctx.font = "30px Arial";
     var x = ScreenResolution.width / 2;
     var y = ScreenResolution.height / 2;
+    ctx.font = "30px Arial";
     ctx.strokeText("Hello World", x, y);
+    //ctx.fillRect(x,y,75,75);
+    document.getElementById('iterator').addEventListener('click', feedback);
 }
-document.addEventListener("DOMContentLoaded", setupScreen);
+document.addEventListener('DOMContentLoaded', setupScreen);
--- howlround.ts
+++ howlround.ts
@@ -3,7 +3,132 @@ const ScreenResolution = {
 	height : 576
 }
 
+const FilterGain = 1;
+const FilterKernel = [
+	1, 4, 7, 4, 1,
+	4,16,26,16, 4,
+	7,26,41,26, 7,
+	4,16,26,16, 4,
+	1, 4, 7, 4, 1
+].map(x => FilterGain * (x/271));
+
+const KernelRadius = Math.floor(Math.sqrt(FilterKernel.length)/2);
+
+function getPixelPointer(x: number, y: number, imageData: ImageData): number {
+	// this is the number of array cells per pixel. There are four:
+	// red, green, blue, alpha
+	let rowLength: number = imageData.width;	
+	let pixelDataSize: number = 4;
+	return y * (rowLength * pixelDataSize ) + (x * pixelDataSize);
+}
+
+function getPixel(x: number, y: number, canvas: HTMLCanvasElement): number[] {
+	let imageData: ImageData = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
+	let pointer: number = getPixelPointer(x, y, imageData);
+	let red = imageData.data[pointer];
+	let green = imageData.data[pointer + 1];
+	let blue = imageData.data[pointer + 2];
+	let alpha = imageData.data[pointer + 3];
+	return [red, green, blue, alpha];
+}
+
+function getBrightness(pointer: number, imageData: ImageData): number {
+	let alpha = imageData.data[pointer + 3];
+	return alpha;
+}
+
+function setBrightness(pointer: number, imageData: ImageData, brightness: number) {
+	imageData.data[pointer + 3] = brightness;
+}
+
+// this has side effects on the imageData array.
+function addBrightness(pointer: number, imageData: ImageData, brightness: number) {
+	let value = getBrightness(pointer, imageData);
+	value = value + brightness;
+	// clip signal output
+	if (value > 255) {
+		value = 255;
+	}
+	setBrightness(pointer, imageData, value);
+}
+
+function testFilter(xI: number, yI: number, kernel: number[]) {
+
+	let k = 0;
+	for (let y = (yI - KernelRadius); y <= (yI + KernelRadius); y++) {
+		for (let x = (xI - KernelRadius); x <= (xI + KernelRadius); x++) {
+			// only operate when the kernel is not overlapping the
+			// edge of the image
+			if (y >= 0 && x >= 0 && y < ScreenResolution.height && x < ScreenResolution.width) {
+				console.log('(' + x.toString() + ',' + y.toString() + ')' + ' at ' + k.toString());
+			}
+			k++;
+			if (k > kernel.length) {
+				console.log('you fucked up');
+				return;
+			}
+		}
+	}
+}
+
+// This has side effects on the output data array.
+// The 'I' in 'xI' is for impulse.
+function filterPixel(xI: number, yI: number, inputData: ImageData,  kernel: number[], outputData: ImageData) {
+
+	let pointer = getPixelPointer(xI, yI, inputData);
+	let inputVal = getBrightness(pointer, inputData);
+	let k = 0;
+
+	for (let y = (yI - KernelRadius); y <= (yI + KernelRadius); y++) {
+		for (let x = (xI - KernelRadius); x <= (xI + KernelRadius); x++) {
+			// only operate when the kernel is not overlapping the
+			// edge of the image
+			if (y >= 0 && x >= 0 && y < inputData.height && x < inputData.width) {
+				let outputVal = inputVal * kernel[k];
+				pointer = getPixelPointer(x, y, outputData);
+				addBrightness(pointer, outputData, outputVal);
+			}
+			k++;
+			if (k > kernel.length) {
+				console.log('you fucked up');
+				return;
+			}
+
+		}
+	}
+}
+
+function convolution(inputData: ImageData, kernel: number[], outputData: ImageData) {
+	let x = 0;
+	let y = 0;
+	for (y = 0; y < inputData.height; y++) {
+		for (x = 0; x < inputData.width; x++) {
+			filterPixel(x, y, inputData, kernel, outputData);
+
+			//let pointer = getPixelPointer(x, y, inputData);
+			//let currentValue = getBrightness(pointer, inputData);
+			//setBrightness(pointer, outputData, currentValue);
+			//addBrightness(pointer, outputData, 10); 
+
+		}
+	}
+}
+
+function feedback() {
+	console.log('feeding back...');
+	let tv = <HTMLCanvasElement> document.getElementById("tvscreen");
+	let ctx = tv.getContext("2d");
+
+	let thisIteration = ctx.getImageData(0, 0, tv.width, tv.height);
+	let nextIteration = ctx.createImageData(tv.width, tv.height);
+	convolution(thisIteration, FilterKernel, nextIteration);
+
+	ctx.putImageData(nextIteration, 0, 0);
+}
+
 function setupScreen() {
+	// initial content
+	//
 	let tv = <HTMLCanvasElement> document.getElementById("tvscreen");
 	// Note that for the canvas element, width and height
 	// are *different* to the css/style width and height.
@@ -11,13 +136,14 @@ function setupScreen() {
 	tv.height = ScreenResolution.height;
 
 	let ctx = tv.getContext("2d");
-	ctx.font = "30px Arial";
 	let x = ScreenResolution.width / 2;
 	let y = ScreenResolution.height / 2;
-
-	ctx.strokeText("Hello World",x,y);
+	
+	ctx.font = "30px Arial";
+	ctx.strokeText("Hello World", x, y);
+	//ctx.fillRect(x,y,75,75);
+	document.getElementById('iterator').addEventListener('click', feedback);
 }
 
-document.addEventListener("DOMContentLoaded", setupScreen);
-
+document.addEventListener('DOMContentLoaded', setupScreen);
 
